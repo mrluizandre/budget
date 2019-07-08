d70e3bbda3fe2b530e55a19dd83f68af9cd86ae7
Andre Costa <mr.luizandre@gmail.com>
2019-07-08 16:44:27 -0300

    Change category field to enter value (budgeted to balance) and add visual improvements

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
 
 
   def budget
-    Category.budget(params[:balance].to_f - @category.balance,@category)
+    Category.budget(params[:budgeted].to_f - @category.budgeted,@category)
     redirect_to root_path
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 	def badge_color(amount)
 		if amount > 0
 			'badge-success'
-		elsif amount = 0
+		elsif amount == 0
 			'badge-secondary'
 		elsif amount < 0
-			'badge-warning'
+			'badge-danger'
 		end
 	end
 end
